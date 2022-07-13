171aa639fd2b835fc6088c6628e592d5da058c04
ZhongJunJimmy <zhong.jun.jimmy@gmail.com>
2022-07-13 21:24:58 +0800

    [TASK] set state by hook

--- night-switch/src/App.js
+++ night-switch/src/App.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header/Header';
 import './App.css';
 
-class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      switchState: false
-    };
-    this.switchOnOrOff = this.switchOnOrOff.bind(this);
-  }
+function App(){
+  const [switchState, setSwitchState] = useState(0);
 
-  switchOnOrOff(){
+  const switchOnOrOff = () => {
     //console.log(this);
-    (this.state.switchState)?this.setState({switchState:false}):this.setState({switchState:true});
+    (switchState)?setSwitchState(0):setSwitchState(1);
   }
 
-  render(){
-    return (
+  return (
     <div className="App">
-      <Header clickFunc = {this.switchOnOrOff} switchState = {this.state.switchState} />
+      <Header clickFunc = {switchOnOrOff} switchState = {switchState} />
     </div>
   );
-  }
-  
 }
+
 
 export default App;
